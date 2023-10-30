87480035032bcbbd677e34667723bf348f99782c
mooner1022 <siwol@mooner.dev>
2023-10-30 12:01:58 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# <p align="center">HoYeon-Frontend</p>
+#### <p align="center">기말과제 :)</p>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<section>
+    <div id="sidebar">
+        <div id="profile-info">
+            <img src="assets/hoseo_logo_1.png" alt="profile">
+            <p class="stdnumber">20221000</p>
+            <p class="name">홍길동</p>
+        </div>
+        <div id="profile-buttons">
+            <button>
+                <i class="fa-regular fa-bookmark"></i><br>
+                내 정보
+            </button>
+            <button>
+                <i class="fa-solid fa-right-from-bracket"></i><br>
+                로그아웃
+            </button>
+        </div>
+        <div id="timetable">
+
+        </div>
+    </div>
+</section>
