d1771e2b63cdfa126dfeca38bd6e258512a07a63
mooner1022 <moonmk10221@gmail.com>
2023-11-10 02:20:17 +0900

    Add login implementation

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,9 +1,50 @@
 <script>
 
     export let showPassword = false;
+    /**
+     * @type HTMLFormElement
+     */
+    let form;
 
     function togglePWVisibility() {
         showPassword = !showPassword;
+    }
+
+    function tryLogin() {
+        const formData = new FormData(form);
+
+        fetch("https://server.mooner.dev/hoyeon/api/v1/auth/login", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include',
+            body: JSON.stringify({
+                'id': formData.get("id"),
+                'password': formData.get("password"),
+            }),
+        })
+            .then(resp => {
+                if (!resp.ok)
+                    return {
+                        code: resp.status,
+                        message: resp.text(),
+                    };
+                return resp.json();
+            })
+            .then(resp => {
+                if (resp.code) {
+                    if (resp.code === 401)
+                        alert("계정이 존재하지 않거나 ID/비밀번호가 일치하지 않아요.");
+                    else
+                        throw Error(`HttpStatus ${resp.code}: ${resp.message}`);
+                } else
+                    alert("OK");
+            })
+            .catch(e => {
+                alert("서버와의 통신에 실패했어요. 잠시 후 다시 시도해주세요.");
+                console.error(e);
+            })
     }
 </script>
 <section>
@@ -12,7 +53,7 @@
         <hr>
         <p id="title">로그인</p>
         <p id="desc">호연 서비스에 접근하려면 로그인이 필요합니다.</p>
-        <form>
+        <form bind:this={form}>
             <div>
                 <p>ID (학번)</p>
                 <input name="id" type="text" inputmode="numeric" maxlength="12" placeholder="20201234"/>
@@ -21,17 +62,17 @@
                 <p>비밀번호</p>
                 <div id="pw-wrapper">
                     <input name="password" type={showPassword ? "text" : "password"}/>
-                    <i id="pw-toggle" class="fa-solid" class:fa-eye={showPassword} class:fa-eye-slash={!showPassword} on:click={togglePWVisibility}></i>
+                    <i id="pw-toggle" class="fa-solid c-pointer" class:fa-eye={showPassword} class:fa-eye-slash={!showPassword} on:click={togglePWVisibility}></i>
                 </div>
             </div>
-            <button type="submit">
+            <button type="button" class="c-pointer" on:click={tryLogin}>
                 <i class="fa-solid fa-right-to-bracket"></i>&nbsp;&nbsp;로그인
             </button>
         </form>
-        <div id="reg-wrapper">
+        <div id="reg-wrapper" class="c-pointer">
             <hr><a>회원가입</a><hr>
         </div>
-        <div id="forgot-pw-wrapper">
+        <div id="forgot-pw-wrapper" class="c-pointer">
             <a>비번을 몰?루</a>
         </div>
     </section>
@@ -59,6 +100,10 @@
     position: absolute;
     width: 100%;
     height: 100%;
+  }
+
+  .c-pointer {
+    cursor: pointer;
   }
 
   .login_container {
@@ -147,7 +192,6 @@
           font-family: "Font Awesome 6 Free", serif;
           font-size: 1.2rem;
           color: #9DA8B2;
-          cursor: pointer;
           margin-left: -2.5rem;
           margin-top: 1rem;
         }
@@ -186,7 +230,6 @@
       }
 
       a {
-        cursor: pointer;
         flex: 1;
         text-align: center;
       }
@@ -199,7 +242,6 @@
 
       a {
         color: #6692FF;
-        cursor: pointer;
       }
     }
   }
