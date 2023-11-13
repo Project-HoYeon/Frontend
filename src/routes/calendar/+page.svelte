2203f6fada95c26a2451139ac9eec58468a4c72b
mooner1022 <siwol@mooner.dev>
2023-11-13 11:28:01 +0900

    Cleanup code and separate styles

--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -1,12 +1,8 @@
-
-
-
 <script>
-  
+    import "./styles.scss";
 
-    
+    /** @type Schedules */
     const data = [
-      
         {
             date : "2023-02-06 ~ 2023-02-10",
             context : "1학기 수강신청 기간"
@@ -14,7 +10,6 @@
         {
             date : "2023-02-13 ~ 2023-02-17",
             context :"1학기 등록 및 일반휴학 접수 기간",
-
         },
         {
             date : "2023-02-27 ~ 2023-02-27",
@@ -48,7 +43,6 @@
             date : "2023-07-03 ~ 2023-07-05",
             context : "성적정정 기간"
         },
-       
         {
             date : "2023-07-31 ~ 2023-08-04",
             context : "2학기 복학원서 접수 기간"
@@ -92,7 +86,7 @@
         {
             date : "2023-10-24 ~ 2023-10-24",
             context : "학기 1/2선"
-        },  
+        },
         {
             date : "2023-11-10 ~ 2023-11-10",
             context : "학기 2/3선"
@@ -101,7 +95,7 @@
             date : "2023-12-01 ~ 2023-12-28",
             context : "성적입력 기간 : 12.01.(금) ~ 12.28.(목)"
         },
-    
+
         {
             date : "2023-12-18 ~ 2023-12-18",
             context : "학기종료일"
@@ -143,7 +137,7 @@
             context : "제40회 전기 학위수여식"
         },
         {date : "2024-02-21 ~ 2024-02-22",
-        context : "편입생/재입학생 수강신청 기간: 02.21.(수) ~ 02.22.(목)"
+            context : "편입생/재입학생 수강신청 기간: 02.21.(수) ~ 02.22.(목)"
         },
         {
             date : "2024-02-23 ~ 2024-02-23",
@@ -156,48 +150,31 @@
         {
             date : "2024-02-26 ~ 2024-02-26",
             context : "2024학년도 1학기 개강"
-        }
+        },
+    ];
 
-    ]
-    // @ts-ignore
-    import { onMount } from 'svelte';
-
+    const maxVisible = 5;
+    let nowVisible = maxVisible;
 
     let year = new Date().getFullYear();
-    let month = new Date().getMonth() + 1; 
-    let schedule = getSchedule(year, month);
+    let month = new Date().getMonth() + 1;
 
-    
-    onMount(() => {
-         schedule = getSchedule(year, month);
-    });
+    /** @type Schedules */
+    let schedules;
+    /** @type CalendarDates */
+    let calendarDates;
+    /** @type Schedules */
+    let visibleSchedules;
 
-    // @ts-ignore
     /**
-   * @type {any[]}
-   */
-    let calendarDays;
-
-   // @ts-ignore
-    // @ts-ignore
-     // @ts-ignore
-      // @ts-ignore
-       // @ts-ignore
-        // @ts-ignore
-          $: {
-        calendarDays = generateCalendarDays(year, month);
-    }
-
-    // @ts-ignore
+     * Generate date objects of month in calendar
+     * @param {number} year
+     * @param {number} month
+     */
     function generateCalendarDays(year, month) {
-        let days = [];
-        const firstDayOfMonth = new Date(year, month - 1, 1);
-        const firstDayIndex = firstDayOfMonth.getDay();
+        const days = [];
+        const firstDayIndex = new Date(year, month - 1, 1).getDay();
         const lastDayOfMonth = new Date(year, month, 0).getDate();
-
-        // @ts-ignore
-        // @ts-ignore
-        
 
         for (let i = 0; i < firstDayIndex; i++) {
             days.push({
@@ -205,230 +182,134 @@
                 check : false,
             });
         }
-      
-    
-        
+
         for (let day = 1; day <= lastDayOfMonth; day++) {
-        let eventExists = schedule.some(event => {
-            const [startStr, endStr] = event.date.split(' ~ ');
-            const startDate = new Date(startStr);
-            const endDate = new Date(endStr);
-            return new Date(year, month-1, day + 1) >= startDate && new Date(year, month-1, day) <= endDate;
-        });
-        days.push({ day, check: eventExists });
-    }
+            let eventExists = schedules.some(event => {
+                const [startStr, endStr] = event.date.split(' ~ ');
+                const startDate = new Date(startStr);
+                const endDate = new Date(endStr);
+                return new Date(year, month - 1, day + 1) >= startDate && new Date(year, month - 1, day) <= endDate;
+            });
+            days.push({ day, check: eventExists });
+        }
         return days;
     }
-   
- 
-    // @ts-ignore
-    function getSchedule(year, month){
-         const date = data.filter((item)=> {
-            const [start, end] = item.date.split(' ~ ').map(d => new Date(d));
-            return ((start.getMonth() + 1 === month) && (start.getFullYear() === year) || 
-                    ((end.getMonth() + 1 === month)  && end.getFullYear() == year))
-        })
-       return date;
-        
-    }
-   
-    // @ts-ignore
-    function changeYear(direction){
 
-        year += (direction === 'up' ? 1 : -1);
+    /**
+     *
+     * @param {number} year
+     * @param {number} month
+     */
+    function getSchedulesOf(year, month){
+        return data.filter((item) => {
+            const [start, end] = item.date
+                .split(' ~ ')
+                .map(d => new Date(d));
+
+            return ((start.getMonth() + 1 === month) && (start.getFullYear() === year) ||
+                ((end.getMonth() + 1 === month) && end.getFullYear() === year));
+        });
+    }
+
+    function nextYear() {
+        updateYear(1);
+    }
+
+    function prevYear() {
+        updateYear(-1);
+    }
+
+    /**
+     * Update year of calendar
+     * @param {number} diff Difference from year now
+     */
+    function updateYear(diff) {
+        year = Math.max(2023, Math.min(year + diff, 2024));
+        updateSchedule();
+    }
+
+    function nextMonth() {
+        updateMonth(1);
+    }
+
+    function prevMonth() {
+        updateMonth(-1);
+    }
+
+    /**
+     *
+     * @param {number} diff
+     */
+    function updateMonth(diff) {
+        if ((month += diff) > 12) {
+            month = 1;
+            year++;
+        }
+        else if (month < 1) {
+            month = 12;
+            year--;
+        }
         year = Math.max(2023, Math.min(year, 2024));
-        schedule = getSchedule(year, month);
+        updateSchedule();
     }
-    
-   
-    // @ts-ignore
-    function changeMonth(direction){
-        
-        month += (direction === 'up' ? 1 : -1);
-        if (month > 12) { month = 1; year++; }
-        else if (month < 1) { month = 12; year--; }
-        year = Math.max(2023, Math.min(year, 2024));
-        schedule = getSchedule(year, month);
+
+    function updateSchedule() {
+        schedules = getSchedulesOf(year, month);
+        calendarDates = generateCalendarDays(year, month);
+        visibleSchedules = schedules.slice(0, nowVisible);
     }
-    
 
-const maxVisible = 5;
+    function showMore() {
+        nowVisible = schedules.length;
+    }
 
-let visibleCount = maxVisible;
-// @ts-ignore
+    updateSchedule();
 
-$: visibleSchedule = schedule.slice(0, visibleCount);
-
-function showMore() {
-    visibleCount = schedule.length;
-   
-}
-
+    /**
+     * @typedef {Schedule[]} Schedules
+     *
+     * @typedef {{date: string, context: string}} Schedule
+     *
+     * @typedef {CalendarDate[]} CalendarDates
+     *
+     * @typedef {{day: number|string, check:boolean}} CalendarDate
+     */
 </script>
 <section>
-    <div class="left-container">
+    <div class="container">
         <div class="controls">
-            <div class="year-display">  
-                <button class="arrow" on:click={() => changeYear('up')}><i class="fas fa-angle-up" style="color: #007bff; "></i></button>
+            <div class="year-display">
+                <button class="arrow" on:click={nextYear}><i class="fas fa-angle-up" style="color: #007bff; "></i></button>
                 <div class="date">{year}년</div>
-                <button class="arrow" on:click={() => changeYear('down')}><i class="fas fa-angle-down" style="color : #007bff; "></i></button>
+                <button class="arrow" on:click={prevYear}><i class="fas fa-angle-down" style="color : #007bff; "></i></button>
             </div>
             <div class="month-display">
-                <button class="arrow" on:click={() => changeMonth('up')}><i class="fas fa-angle-up" style="color : #007bff; "></i></button>
+                <button class="arrow" on:click={nextMonth}><i class="fas fa-angle-up" style="color : #007bff; "></i></button>
                 <div class="date">{month}월</div>
-                <button class="arrow" on:click={() => changeMonth('down')}><i class="fas fa-angle-down" style="color : #007bff;"></i></button>
+                <button class="arrow" on:click={prevMonth}><i class="fas fa-angle-down" style="color : #007bff;"></i></button>
             </div>
         </div>
         <div class="calendar">
             <div class="calendar-grid">
-                {#each calendarDays as item}
-                    <div class="calendar-day" style ="background-color : {item.check ? "#007bff;" : ""};" >{item.day}</div>
+                {#each calendarDates as date}
+                    <div class="calendar-day" class:highlighted={date.check}>{date.day}</div>
                 {/each}
             </div>
         </div>
     </div>
-    <div class="right-container">
-        <h1 class = "main_title">학사일정</h1>
-
+    <div class="container">
+        <h1 class="main-title">학사일정</h1>
         <ul>
-            {#each visibleSchedule  as item, index (item.date + '-' + index)}
-            <li><strong>{item.date}</strong>: <sapn>{item.context}</sapn></li>
-            {/each}
-            {#if schedule.length > visibleCount}
-            <button on:click={showMore} class="show-more" >더보기</button>
+            {#if visibleSchedules === undefined}
+                <p>로드중...</p>
+            {:else}
+                {#each visibleSchedules as schedule, index (schedule.date + '-' + index)}
+                    <li><strong>{schedule.date}</strong>: <span>{schedule.context}</span></li>
+                {/each}
+            {/if}
+            {#if schedules.length > nowVisible}
+                <button on:click={showMore} class="show-more" >더보기</button>
             {/if}
         </ul>
-        
     </div>
 </section>
-
-<style>
-    section {
-    position: absolute;
-    display: flex;
-    align-items: start;
-    justify-content: center;
-    box-sizing: border-box;
-    width: 100%;
-    height: 100%;
-    background-color: #f5f5f5;
-    padding: 1.25rem; 
-}
-
-.left-container, .right-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    flex: 1;
-    margin: 2.5rem; 
-    background-color: #fff;
-    padding: 1.25rem; 
-    border-radius: 0.625rem; 
-    box-shadow: 0 0.25rem 0.5rem rgba(0, 0, 0, 0.1); 
-}
-
-.main_title {
-    font-size: 2.5rem; 
-    color: #333;
-    text-align: center;
-    margin-bottom: 1.25rem; 
-}
-
-.controls {
-    display: flex;
-    justify-content: center;
-    padding: 0.625rem; 
-    margin-bottom: 1.25rem; 
-}
-
-.year-display, .month-display {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
-
-.arrow {
-    cursor: pointer;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 2rem;
-    background-color: white;
-    border: none;
-    border-radius: 0.5rem; 
-}
-
-.date {
-    font-size: 1.5rem; 
-    font-weight: bold;
-    margin: 0.5rem;
-}
-
-.calendar {
-    max-width: 21.875rem;
-    background-color: #fff;
-    border-radius: 0.625rem; 
-    box-shadow: 0 0.25rem 0.5rem rgba(0, 0, 0, 0.1); 
-    padding: 1.25rem;
-    text-align: center;
-}
-
-.calendar-grid {
-    display: grid;
-    grid-template-columns: repeat(7, 1fr);
-    gap: 0.3125rem;
-}
-
-.calendar-day {
-    padding: 0.625rem;
-    border-radius: 0.3125rem; 
-    background-color: #f9f9f9;
-    color: #333;
-    font-weight: bold;
-    transition: background-color 0.2s ease-in-out;
-}
-
-ul {
-    list-style: none;
-    margin-top: 1rem;
-}
-
-li {
-    background-color: #fff;
-    margin-bottom: 0.3125rem; 
-    padding: 0.75rem; 
-    border-radius: 0.5rem; 
-    box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.1); 
-    transition: background-color 0.2s;
-}
-
-li:hover {
-    background-color: #e9ecef;
-}
-
-li strong {
-    color: #007bff;
-    margin-right: 0.5rem; 
-}
-
-.show-more {
-    padding: 0.5rem 1rem; 
-    font-size: 1rem; 
-    color: #fff;
-    background-color: #007bff;
-    border: none;
-    border-radius: 0.3rem; 
-    margin-top: 0.5rem; 
-    cursor: pointer;
-    transition: background-color 0.2s;
-}
-
-.show-more:hover {
-    background-color: #0056b3;
-}
-
-</style>
-
-
-
-
